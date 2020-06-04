fcc9f23461992668de16e3f545b95efc1c982a52
Rick Messak <66070353+RickMessak@users.noreply.github.com>
2020-06-04 16:11:30 +0200

    Add files via upload

---
+++ Engine/LivingCreatures.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+
+    // LivingCreatures is the BaseClass of the derived (child) classes Monster and Player
+    public class LivingCreatures
+    {
+
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
+
+        public LivingCreatures (int currentHitPoints, int maximumHitPoints)
+        {
+            CurrentHitPoints = currentHitPoints;
+            MaximumHitPoints = maximumHitPoints;
+
+        }
+
+    }
+}
